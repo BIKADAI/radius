d5a9ba444f8860b8cad1c48c3f34e291c97c5487
BIKADAI <radius.bikadai@gmail.com>
2020-04-08 16:08:33 +0100

    fixed bug

--- src/estimator.js
+++ src/estimator.js
@@ -13,47 +13,47 @@ const covid19ImpactEstimator = (data) => {
   output.data = data;
   output.impact.currentlyInfected = data.reportedCases * 10;
   output.severeImpact.currentlyInfected = data.reportedCases * 50;
-  output.impact.infectionsByRequestedTime = data.impact.currentlyInfected * 2 ** factor;
-  output.severeImpact.infectionsByRequestedTime = data.impact.currentlyInfected * 2 ** factor;
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 2 ** factor;
+  output.severeImpact.infectionsByRequestedTime = output.impact.currentlyInfected * 2 ** factor;
   /**
    * challenge 1 end , and  number 2  start
    */
-  const percentageInfectionsImpact = (data.impact.infectionsByRequestedTime * 15) / 100;
+  const percentageInfectionsImpact = (output.impact.infectionsByRequestedTime * 15) / 100;
   output.impact.severeCasesByRequestedTime = Math.floor(percentageInfectionsImpact);
-  const PersentageInfectionSevere = (data.severeImpact.infectionsByRequestedTime * 15) / 100;
+  const PersentageInfectionSevere = (output.severeImpact.infectionsByRequestedTime * 15) / 100;
   output.severeImpact.severeCasesByRequestedTime = Math.floor(PersentageInfectionSevere);
-  let beds = (data.totalHospitalBeds - data.impact.severeCasesByRequestedTime * 35) / 100;
+  let beds = (data.totalHospitalBeds - output.impact.severeCasesByRequestedTime * 35) / 100;
   output.impact.hospitalBedsByRequestedTime = Math.floor(beds);
-  beds = (data.totalHospitalBeds - data.severeImpact.severeCasesByRequestedTime * 35) / 100;
+  beds = (data.totalHospitalBeds - output.severeImpact.severeCasesByRequestedTime * 35) / 100;
   output.severeImpact.hospitalBedsByRequestedTime = Math.floor(beds);
   /**
    * end challenge 2, start challenge 3
    */
-  let casesForVentByRequestedTimeImpact = (data.severeImpact.severeCasesByRequestedTime * 2) / 100;
-  let casesForICUByRequestedTimeImpact = (data.impact.severeCasesByRequestedTime * 5) / 100;
-  let casesForVentByRequestedTimeSevere = (data.severeImpact.severeCasesByRequestedTime * 2) / 100;
-  let casesForICUByRequestedTimeSevere = (data.severeImpact.severeCasesByRequestedTime * 5) / 100;
+  let casesForVentByRequestedTimeImpact = (output.severeImpact.severeCasesByRequestedTime * 2) / 100;
+  let casesForICUByRequestedTimeImpact = (output.impact.severeCasesByRequestedTime * 5) / 100;
+  let casesForVentByRequestedTimeSevere = (output.severeImpact.severeCasesByRequestedTime * 2) / 100;
+  let casesForICUByRequestedTimeSevere = (output.severeImpact.severeCasesByRequestedTime * 5) / 100;
   casesForICUByRequestedTimeImpact = Math.floor(casesForICUByRequestedTimeImpact);
   casesForVentByRequestedTimeImpact = Math.floor(casesForVentByRequestedTimeImpact);
   casesForVentByRequestedTimeSevere = Math.floor(casesForVentByRequestedTimeSevere);
   casesForICUByRequestedTimeSevere = Math.floor(casesForICUByRequestedTimeSevere);
-  data.impact.casesForICUByRequestedTime = casesForICUByRequestedTimeImpact;
-  data.impact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeImpact;
-  data.severeImpact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeSevere;
-  data.severeImpact.casesForICUByRequestedTime = casesForICUByRequestedTimeSevere;
+  output.impact.casesForICUByRequestedTime = casesForICUByRequestedTimeImpact;
+  output.impact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeImpact;
+  output.severeImpact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeSevere;
+  output.severeImpact.casesForICUByRequestedTime = casesForICUByRequestedTimeSevere;
   /**
    * economy loss
    */
-  let impactEconomyLost = data.impact.infectionsByRequestedTime;
+  let impactEconomyLost = output.impact.infectionsByRequestedTime;
   impactEconomyLost *= data.region.avgDailyIncomeInUSD;
-  impactEconomyLost *= data.impact.avgDailyIncomePopulation;
+  impactEconomyLost *= data.region.avgDailyIncomePopulation;
   impactEconomyLost *= durationIndays;
   data.impact.dollarsInFlight = impactEconomyLost;
-  impactEconomyLost = data.severeImpact.infectionsByRequestedTime;
+  impactEconomyLost = output.severeImpact.infectionsByRequestedTime;
   impactEconomyLost *= data.region.avgDailyIncomeInUSD;
-  impactEconomyLost *= data.severeImpact.avgDailyIncomePopulation;
+  impactEconomyLost *= output.severeImpact.avgDailyIncomePopulation;
   impactEconomyLost *= durationIndays;
-  data.severeImpact.dollarsInFlight = impactEconomyLost;
+  output.severeImpact.dollarsInFlight = impactEconomyLost;
 
   return output;
 };
