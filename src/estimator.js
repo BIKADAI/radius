89ffe2869e0f9b6d4c66220361eeeb0bce669475
BIKADAI <radius.bikadai@gmail.com>
2020-04-09 12:21:10 +0100

    solved bug on challenge 1,2

--- src/estimator.js
+++ src/estimator.js
@@ -27,12 +27,12 @@ const covid19ImpactEstimator = (data) => {
   /**
    * ok
    * challenge number 1 start here
-   *  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** (fa));
-  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** fa);
    */
   output.data = data;
-  output.impact.currentlyInfected = data.reportedCases * 10 * fa;
+  output.impact.currentlyInfected = data.reportedCases * 10;
   output.severeImpact.currentlyInfected = data.reportedCases * 50;
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * (2 ** (fa));
+  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * (2 ** fa);
   /**
    * challenge 1 end , and  number 2  start
    */
@@ -47,6 +47,33 @@ const covid19ImpactEstimator = (data) => {
   /**
    * end challenge 2, start challenge 3
    */
+  let casesForVentByRequestedTimeImpac = (output.impact.infectionsByRequestedTime * 2) / 100;
+  let casesForICUByRequestedTimeImpact = (output.impact.infectionsByRequestedTime * 5) / 100;
+  let casesForVentByRequestedTimeSever = (output.severeImpact.infectionsByRequestedTime * 2) / 100;
+  let casesForICUByRequestedTimeSevere = (output.severeImpact.infectionsByRequestedTime * 5) / 100;
+  casesForICUByRequestedTimeImpact = Math.floor(casesForICUByRequestedTimeImpact);
+  casesForVentByRequestedTimeImpac = Math.floor(casesForVentByRequestedTimeImpac);
+  casesForVentByRequestedTimeSever = Math.floor(casesForVentByRequestedTimeSever);
+  casesForICUByRequestedTimeSevere = Math.floor(casesForICUByRequestedTimeSevere);
+  output.impact.casesForICUByRequestedTime = casesForICUByRequestedTimeImpact;
+  output.impact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeImpac;
+  output.severeImpact.casesForVentilatorsByRequestedTime = casesForVentByRequestedTimeSever;
+  output.severeImpact.casesForICUByRequestedTime = casesForICUByRequestedTimeSevere;
+  /**
+   * economy loss
+   */
+  let impactEconomyLost = output.impact.infectionsByRequestedTime;
+  if (impactEconomyLost > data.region.population) impactEconomyLost = data.region.population;
+  impactEconomyLost *= data.region.avgDailyIncomeInUSD;
+  impactEconomyLost *= data.region.avgDailyIncomePopulation;
+  impactEconomyLost *= durationIndays;
+  output.impact.dollarsInFlight = impactEconomyLost;
+  impactEconomyLost = output.severeImpact.infectionsByRequestedTime;
+  impactEconomyLost *= data.region.avgDailyIncomeInUSD;
+  impactEconomyLost *= data.region.avgDailyIncomePopulation;
+  impactEconomyLost *= durationIndays;
+  output.severeImpact.dollarsInFlight = impactEconomyLost;
+
   return output;
 };
 export default covid19ImpactEstimator;
