5a01412721109b3a9530a79ebdedeb8cb3a88f08
BIKADAI <radius.bikadai@gmail.com>
2020-04-09 08:48:20 +0100

    replace math.floor by math.trunc for bed availability

--- src/estimator.js
+++ src/estimator.js
@@ -23,9 +23,9 @@ const covid19ImpactEstimator = (data) => {
   const PersentageInfectionSevere = (output.severeImpact.infectionsByRequestedTime * 15) / 100;
   output.severeImpact.severeCasesByRequestedTime = PersentageInfectionSevere;
   let beds = (data.totalHospitalBeds * 35) / 100 - output.impact.severeCasesByRequestedTime;
-  output.impact.hospitalBedsByRequestedTime = Math.floor(beds);
+  output.impact.hospitalBedsByRequestedTime = Math.trunc(beds);
   beds = (data.totalHospitalBeds * 35) / 100 - output.severeImpact.severeCasesByRequestedTime;
-  output.severeImpact.hospitalBedsByRequestedTime = Math.floor(beds);
+  output.severeImpact.hospitalBedsByRequestedTime = Math.trunc(beds);
   /**
    * end challenge 2, start challenge 3
    */
